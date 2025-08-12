0a2297f05b5cf81bbe06eeb2131b1e3f08fdc69c
Arto Gahr <artogahr@gmail.com>
2025-08-12 23:12:20 +0200

    Helix theming

--- modules/helix.nix
+++ modules/helix.nix
@@ -1,8 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.helix = {
     enable = true;
-    package = pkgs.evil-helix;
+    settings = {
+      theme = lib.mkForce "catppuccin-mocha-transparent";
+    };
+    themes = {
+      "catppuccin-mocha-transparent" = {
+        # Inherit all other styles from the original catppuccin-mocha theme
+        inherits = "catppuccin-mocha";
+        # Set the UI background to be transparent by leaving it empty
+        "ui.background" = { };
+      };
+    };
   };
 }
