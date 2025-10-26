b3a93f772010976027db66263a5b672fe8259aa9
Arto Gahr <artogahr@gmail.com>
2025-10-26 21:37:45 +0100

    plasma shortcuts config

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -11,8 +11,12 @@
       key = "Ctrl+Alt+T";
       command = "wezterm";
     };
-    kwin.effects.dimInactive.enable = true;
-    kwin.effects.blur.enable = true;
+    kwin = {
+      effects = {
+        dimInactive.enable = true;
+        blur.enable = true;
+      };
+    };
     powerdevil.AC = {
       powerButtonAction = "sleep";
       dimDisplay.enable = true;
@@ -22,5 +26,25 @@
     };
     session.sessionRestore.restoreOpenApplicationsOnLogin = "whenSessionWasManuallySaved";
     windows.allowWindowsToRememberPositions = true;
+    shortcuts = {
+      kwin = {
+        "Toggle Overview" = "Meta+Tab";
+        "Switch Window Down" = "Meta+J";
+        "Switch Window Left" = "Meta+H";
+        "Switch Window Right" = "Meta+L";
+        "Switch Window Up" = "Meta+K";
+        "Close Window" = "Meta+Q";
+        "Move Window Left" = "Meta+Left";
+        "Quick Tile Window to the Right" = "Meta+Right";
+        "Quick Tile Window to the Left" = "Meta+Left";
+        "Quick Tile Window to the Top" = "Meta+Up";
+        "Quick Tile Window to the Down" = "Meta+Down";
+        "Move Window to the Center" = "Meta+C";
+        "Move Window One Screen Down" = "Meta+Shift+Down";
+        "Move Window One Screen Up" = "Meta+Shift+Up";
+        "Switch One Desktop Up" = "Meta+Ctrl+Left";
+        "Switch One Desktop Down" = "Meta+Ctrl+Right";
+      };
+    };
   };
 }
